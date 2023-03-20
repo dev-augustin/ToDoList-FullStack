b75ae6416cb865fa90e922225ca131fe95ec64c4
PriyaAugustin <raugustin@teksystems.com>
2023-03-19 23:53:13 -0500

    Implemented register user and login user with hardcoded credentials

--- todoapp-frontend/src/components/users/Login.js
+++ todoapp-frontend/src/components/users/Login.js
@@ -1,33 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+  let navigate = useNavigate();
+  const [user, setUser] = useState({ userName: "", password: "" });
+  const { userName, password } = user;
+  const onInputChange = (e) => {
+    console.log("e name: " + e.target.name + " vlaue: " + e.target.value);
+    setUser({ [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    console.log("I am here");
+    console.log("eister: " + e.target[1].value);
+
+    e.preventDefault();
+    if (e.target[0].value == "WHO" && e.target[1].value == "test") {
+      console.log("asfsfsaf " + (await axios.get("http://localhost:8080/todo/3")).data.todo);
+      navigate("/");
+    } else {
+      alert("Wrong credentials");
+    }
+
+    // await axios.get("http://localhost:8080/todos");
+  };
+
   return (
     <>
       <div>
-        <form>
+        <form onSubmit={(e) => onSubmit(e)}>
           <div>
             <label>User Name</label>
             <input
               className="shadow appearance-none border rounded p-8 m-8 w-500 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
               name="userName"
+              value={userName}
+              onChange={(e) => onInputChange(e)}
             />
             <label>Password</label>
             <input
               className="shadow appearance-none border rounded p-8 m-8 w-500 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
               name="password"
+              value={password}
+              onChange={(e) => onInputChange(e)}
             />
           </div>
           <div>
-            <Link
+            <button
+              type="submit"
+              className=" m-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            >
+              Submit
+            </button>
+            {/* <Link
               to="/"
               className=" m-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold h over:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
               type="submit"
             >
               Login
-            </Link>
+            </Link> */}
             <Link
               to="/register"
               className=" m-4 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
--- todoapp-frontend/src/components/users/Register.js
+++ todoapp-frontend/src/components/users/Register.js
@@ -1,34 +1,61 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
+  let navigate = useNavigate();
+  const [user, setUser] = useState({ name: "", userName: "", password: "", email: "" });
+  const { name, userName, password, email } = user;
+  const onInputChange = (e) => {
+    console.log("e name: " + e.target.name + " vlaue: " + e.target.value);
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const registerUser = async (e) => {
+    console.log("eister: " + e);
+    e.preventDefault();
+
+    await axios.post("http://localhost:8080/user", user);
+    alert("User account created successfully");
+
+    navigate("/todoHome");
+  };
+
   return (
     <>
       <h4>Sign Up</h4>
-      <form>
+      <form onSubmit={(e) => registerUser(e)}>
         <label>Full Name</label>
         <input
           className="shadow appearance-none border rounded p-8 m-8 w-500 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="name"
+          value={name}
+          onChange={(e) => onInputChange(e)}
         />
         <label>User Name</label>
         <input
           className="shadow appearance-none border rounded p-8 m-8 w-500 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="userName"
+          value={userName}
+          onChange={(e) => onInputChange(e)}
         />
         <label>Password</label>
         <input
           className="shadow appearance-none border rounded p-8 m-8 w-500 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="password"
+          value={password}
+          onChange={(e) => onInputChange(e)}
         />
         <label>Email</label>
         <input
           className="shadow appearance-none border rounded p-8 m-8 w-500 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           name="email"
+          value={email}
+          onChange={(e) => onInputChange(e)}
         />
 
         <button
